657002abf43bd920bf2700b82560648000f3d954
EngrSimon <001diamondgrafix@gmail.com>
2023-10-19 20:05:17 -0700

    ./101-print_numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,16 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int i = n;
 
-	n1 = n;
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+		_putchar(45);
+		i = -i;
 	}
-	if (n1 / 10 != 0)
+	if (i / 10)
 	{
-		print_number(n1 / 10);
+		print_number(i / 10);
 	}
-	_putchar((n1 & 10) + '0');
+	_putchar(i % 10 + '0');
 }
